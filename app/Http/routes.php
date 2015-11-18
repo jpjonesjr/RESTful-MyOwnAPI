497ab0dc9f0d662114d4f997d090333efdd1f12d
JP Jones <jpjones@autoweb.com>
2015-11-18 11:18:45 -0600

    Adding routes
    Updating routes described on lesson 15

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+/*
 Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
@@ -19,3 +19,10 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+*/
+
+Route::resource('makers','MakerController', ['except' => ['create', 'edit']]);
+
+Route::resource('vehicles','VehicleController', ['only' => ['index', 'show']]);
+
+Route::resource('makers.vehicles','MakerVehiclesController', ['except' => ['edit', 'create']]);
