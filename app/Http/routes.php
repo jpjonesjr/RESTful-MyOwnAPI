52234e0adbb973bcdc932ce3aae5f2ceb8bb537e
JP Jones <jpjones@autoweb.com>
2015-11-18 11:50:00 -0600

    Creating our controllers
    Creating controllers for every route to our API using only the necessary
    methods

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,6 +23,6 @@ Route::controllers([
 
 Route::resource('makers','MakerController', ['except' => ['create', 'edit']]);
 
-Route::resource('vehicles','VehicleController', ['only' => ['index', 'show']]);
+Route::resource('vehicles','VehicleController', ['only' => ['index']]);
 
 Route::resource('makers.vehicles','MakerVehiclesController', ['except' => ['edit', 'create']]);
