9d3eb4f410467168c8dcdc3c7b3491e6d7a38ff0
JP Jones <jpjones@autoweb.com>
2015-11-26 11:58:41 -0600

    Adding the versioning to the API
    - Creating a group in the routes to add the version prefix.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,8 +21,11 @@ Route::controllers([
 ]);
 */
 
-Route::resource('makers','MakerController', ['except' => ['create', 'edit']]);
+Route::group(array('prefix' => 'api/v1.1'), function()
+{
+	Route::resource('makers','MakerController', ['except' => ['create', 'edit']]);
 
-Route::resource('vehicles','VehicleController', ['only' => ['index']]);
+	Route::resource('vehicles','VehicleController', ['only' => ['index']]);
 
-Route::resource('makers.vehicles','MakerVehiclesController', ['except' => ['edit', 'create']]);
+	Route::resource('makers.vehicles','MakerVehiclesController', ['except' => ['edit', 'create']]);
+});	
